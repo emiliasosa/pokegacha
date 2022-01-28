859e46507d31e116d218c5fd4c9ad32ce6ac6f4f
emiliasosa <emi.sosato@gmail.com>
2022-01-28 00:21:35 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,43 +1,53 @@
 let dinero = parseInt(prompt("Ingrese su dinero, 1 jugada tiene el valor de 3 usd"));
 
+/*let dinero;
+
+do{
+    dinero = parseInt(prompt("Ingrese su dinero, 1 jugada tiene el valor de 3 usd"));
+}while(dinero != isNaN);*/
+
 let jugadas = (dinero) =>{
     let totalDeJugadas = 0;
 
-    totalDeJugadas = dinero / 3;
-
-    return totalDeJugadas
+    if(dinero % 3 == 0){
+        totalDeJugadas = dinero / 3;
+        return totalDeJugadas
+    }else{
+        return false;
+    }
 }
 
 let tiros = jugadas(dinero);
 
+let gacha = () => {
+    let aceptar = prompt("Deseas realizar la jugada?")
 
-
-while(dinero >= 3 || dinero <= 3){
-
-    let gacha = () => {
-        for (i = 0; i < tiros; i--) {
-            let aceptar = prompt("Deseas realizar la jugada?")
-    
-            if(aceptar == "si"){
-                alert("Haz sacado un gashapon!")
-            } else {
-                alert("Puedes realizar tu tirada cuando quieras")
+    if(aceptar == "si"){
+        for (let i = 1; i <= tiros; i++) {
+            alert("Haz sacado un gashapon!")
+            
+            if(i < tiros){
+                aceptar = prompt("Deseas realizar la jugada?")
+            }else{
+                alert("Te quedaste sin jugadas")
             }
         }
+    } else {
+        alert("Puedes realizar tu tirada cuando quieras");
     }
-    
-    if(dinero < 3 ){
-        do{
-            dinero = Number(prompt("No te alcanza, debes ingresar mas dinero. Una jugada tiene el valor de 3 usd"));
-        } while (dinero < 3)
-    } 
-
-    if (dinero > 3){
-        alert(`Tienes un total de ${tiros} jugadas`)
-        alert(gacha())
-    } else if (dinero === 3 ){
-        alert("Puedes realizar una jugada")
-        alert(gacha())
-    }
-    
 }
+
+if(dinero < 3 /*|| tiros == false*/){
+    do{
+        dinero = Number(prompt("No te alcanza, debes ingresar mas dinero. Una jugada tiene el valor de 3 usd"));
+    } while (dinero < 3 /*|| tiros == false*/)
+} 
+
+if (tiros > 1){
+    alert(`Tienes un total de ${tiros} jugadas`)
+    alert(gacha())
+} else if (tiros == 1){
+    alert("Puedes realizar una jugada")
+    alert(gacha())
+}
+    
