a944b6746aaf1c33b7a4f55458c736d7ea99e3dd
emiliasosa <emi.sosato@gmail.com>
2022-01-28 22:21:05 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,38 +1,13 @@
-let dinero = 0;
-
-let tiros;
-
-do{
-    dinero = parseInt(prompt("Ingrese su dinero, 1 jugada tiene el valor de 3 usd"));
-}while(/\D/.test(dinero) || dinero == "" || dinero < 3 || tiros == false);
-
-let jugadas = (dinero) =>{
-    let totalDeJugadas = 0;
-
-    if(dinero % 3 == 0){
-        totalDeJugadas = dinero / 3;
-        return totalDeJugadas
-    }else{
-        return false;
-    }
-}
-
-tiros = jugadas(dinero);
-
-
-
 let gacha = () => {
-    let aceptar = prompt("Deseas realizar la jugada?")
+    let aceptar = prompt("Deseas comenzar el juego?")
 
     if(aceptar.toLowerCase() == "si"){
-        for (let i = 1; i <= tiros; i++) {
-            let total = tiros - i
-
-            if(i < tiros && aceptar.toLowerCase() == "si"){
-                aceptar = prompt(`Haz sacado un gashapon! Te quedan ${total} jugadas. Deseas realizar otra jugada?`)
-            }else if (i == tiros && aceptar.toLowerCase() == "si"){
+        for (let i = 1; i <= totalDeJugadas; i++) {
+            if(i < totalDeJugadas && aceptar.toLowerCase() == "si" ){
+                aceptar = prompt(`Haz sacado un gashapon! Te quedan ${totalDeJugadas - i} jugadas. Deseas realizar otra jugada?`)
+            }else if (i == totalDeJugadas && aceptar.toLowerCase() == "si" ){
                 alert("Haz sacado un gashapon! Te quedaste sin jugadas, gracias por jugar!")
-            }else{
+            }else {
                 alert("Puedes realizar tu tirada cuando quieras");
                 break;
             }
@@ -42,10 +17,26 @@ let gacha = () => {
     }
 }
 
-if (tiros > 1){
-    alert(`Tienes un total de ${tiros} jugadas`)
-    gacha()
-} else if (tiros == 1){
+let dinero = 0;
+let totalDeJugadas = 0;
+let cortarDo = false;
+
+do{
+    dinero = parseInt(prompt("Ingrese su dinero, 1 jugada tiene el valor de 3 usd"));
+    if(dinero % 3 == 0 && dinero != 0){
+        totalDeJugadas = dinero / 3;
+        cortarDo = true;
+    } 
+}while(cortarDo == false);
+
+gacha()
+
+
+
+
+
+/*if (totalDeJugadas > 1){
+    alert(`Tienes un total de ${totalDeJugadas} jugadas`)
+} else if (totalDeJugadas == 1){
     alert("Puedes realizar una jugada")
-    gacha()
-}
+}*/
