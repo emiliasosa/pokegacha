9f28d844a7dfcd320d5bf60e4b774a3bb237e046
emiliasosa <emi.sosato@gmail.com>
2022-02-07 22:56:48 -0300

    Create dom.js

---
+++ js/dom.js
@@ -0,0 +1,6 @@
+document.getElementById('jugadas').innerHTML = `Tienes un total de ${totalDeJugadas} jugadas`;
+
+document.getElementById('premio').innerHTML = `Ganaste: ${allObjetc.probabilidad}`
+
+document.getElementById('skip').innerHTML = `${saltear}`
+
