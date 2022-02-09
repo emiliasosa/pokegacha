d7586d87ece14d03cac20ae9ca0be683648177fb
emiliasosa <emi.sosato@gmail.com>
2022-02-09 01:24:39 -0300

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -1,6 +1,8 @@
-let contenido = document.getElementById('contendor')
+let btn = document.getElementById('Map')
+btn.addEventListener('click', gacha)
 
-contenido.innerHTML = `<h3>Tienes un total de ${totalDeJugadas} jugadas</h3>
-                    <h2>Ganaste: ${allObjetc.probabilidad}</h2> 
-                    <p>${saltear}</p>`
+let dine = document.getElementById('pago');
+dine.addEventListener('submit', solicitarPago)
 
+document.getElementById('cantidad').innerHTML = `Tienes un total de ${totalDeJugadas} jugadas`;
+document.getElementById('premios').innerHTML = `Aun no tienes premios`;
