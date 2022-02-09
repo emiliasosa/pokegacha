bba364e1f7da5c911e2d6d714094319a79d16d1f
emiliasosa <emi.sosato@gmail.com>
2022-02-09 01:24:41 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,30 @@
 let gacha = () => {
-    let aceptar = prompt("Deseas comenzar el juego?")
-
-    if(aceptar.toLowerCase() == "si"){
-        for (let i = 1; i <= totalDeJugadas; i++) {
-            if(i < totalDeJugadas && aceptar.toLowerCase() == "si" ){
-                aceptar = prompt(`Haz sacado un gashapon! Te quedan ${totalDeJugadas - i} jugadas. Deseas realizar otra jugada?`)
-            }else if (i == totalDeJugadas && aceptar.toLowerCase() == "si" ){
-                alert("Haz sacado un gashapon! Te quedaste sin jugadas, gracias por jugar!")
-            }else {
-                saltear = `Puedes realizar tu tirada cuando quieras,aun tienes ${i}`;
-                break;
-            }
+    for (let i = 1; i <= totalDeJugadas; i++) {
+        if(i < totalDeJugadas){
+            alert(`Haz sacado un gashapon! Te quedan ${totalDeJugadas - i} jugadas`);
+            break;
+        }else if (i == totalDeJugadas){
+            document.getElementById('premios').innerHTML = 'Haz sacado un gashapon! Te quedaste sin jugadas, gracias por jugar';
         }
-    } else {
-        alert(`Vuelve en otra ocasion, aun tienes ${totalDeJugadas}`);
     }
 }
+
+let solicitarPago = (e) =>{
+    e.preventDefault();
+    let formulario = e.target
+    
+    dinero = parseInt(formulario.children[0].value);
+
+    if(dinero % 3 == 0 && dinero != 0){
+        totalDeJugadas = dinero / 3;
+        document.getElementById('cantidad').innerHTML = `Tienes un total de ${totalDeJugadas} jugadas`;
+    }else{
+        document.getElementById('cantidad').innerHTML = 'Dinero insuficiente';
+    }
+}
+
 
 let dinero = 0;
 let totalDeJugadas = 0;
 let cortarDo = false;
 let saltear = "";
-
-console.log(saltear)
-
-do{
-    dinero = parseInt(prompt("Ingrese su dinero, 1 jugada tiene el valor de 3 usd"));
-    if(dinero % 3 == 0 && dinero != 0){
-        totalDeJugadas = dinero / 3;
-        cortarDo = true;
-    } 
-}while(cortarDo == false);
-
-gacha()
-
