1c0a12ce606521ca5337deaeab0925778fcaedd9
emiliasosa <emi.sosato@gmail.com>
2022-02-09 01:24:35 -0300

    Update array.js

--- js/array.js
+++ js/array.js
@@ -1,4 +1,4 @@
-class Price{
+class Prize{
     constructor(data){
         this.id = data.id
         this.nombre = data.nombre
@@ -32,20 +32,20 @@ class Price{
 }
 
 const allObjetc = [];
-let buscador = prompt("Que categoria estas buscando?")
+//let buscador = prompt("Que categoria estas buscando?")
 
-const gasha1 = new Object ({id: 1, nombre: "Anana", articulo: "un", img: 40, cantidad: 10, categoria: "frutas"})
-const gasha2 = new Object ({id: 2, nombre: "Sandia", articulo: "una", img: 50, cantidad: 15, categoria: "frutas"})
-const gasha3 = new Object ({id: 3, nombre: "Naranja", articulo: "una", img: 70, cantidad: 20, categoria: "frutas"})
-const gasha4 = new Object ({id: 4, nombre: "Frutilla", articulo: "una", img: 80, cantidad: 15, categoria: "frutas"})
-const gasha5 = new Object ({id: 5, nombre: "Mandarina", articulo: "una", img: 90, cantidad: 20, categoria: "frutas"})
-const gasha6 = new Object ({id: 1, nombre: "Goku", articulo: "a", img: 40, cantidad: 10, categoria: "dragonball"})
-const gasha7 = new Object ({id: 2, nombre: "Trunks", articulo: "a", img: 50, cantidad: 15, categoria: "dragonball"})
-const gasha8 = new Object ({id: 3, nombre: "Bulma", articulo: "a", img: 70, cantidad: 20, categoria: "dragonball"})
-const gasha9 = new Object ({id: 4, nombre: "Vegeta", articulo: "a", img: 80, cantidad: 15, categoria: "dragonball"})
-const gasha10 = new Object ({id: 5, nombre: "Gohan", articulo: "a", img: 90, cantidad: 20, categoria: "dragonball"})
+const gasha1 = new Prize ({id: 1, nombre: "Anana", articulo: "un", img: 40, cantidad: 10, categoria: "frutas"})
+const gasha2 = new Prize ({id: 2, nombre: "Sandia", articulo: "una", img: 50, cantidad: 15, categoria: "frutas"})
+const gasha3 = new Prize ({id: 3, nombre: "Naranja", articulo: "una", img: 70, cantidad: 20, categoria: "frutas"})
+const gasha4 = new Prize ({id: 4, nombre: "Frutilla", articulo: "una", img: 80, cantidad: 15, categoria: "frutas"})
+const gasha5 = new Prize ({id: 5, nombre: "Mandarina", articulo: "una", img: 90, cantidad: 20, categoria: "frutas"})
+const gasha6 = new Prize ({id: 1, nombre: "Goku", articulo: "a", img: 40, cantidad: 10, categoria: "dragonball"})
+const gasha7 = new Prize ({id: 2, nombre: "Trunks", articulo: "a", img: 50, cantidad: 15, categoria: "dragonball"})
+const gasha8 = new Prize ({id: 3, nombre: "Bulma", articulo: "a", img: 70, cantidad: 20, categoria: "dragonball"})
+const gasha9 = new Prize ({id: 4, nombre: "Vegeta", articulo: "a", img: 80, cantidad: 15, categoria: "dragonball"})
+const gasha10 = new Prize ({id: 5, nombre: "Gohan", articulo: "a", img: 90, cantidad: 20, categoria: "dragonball"})
 
 allObjetc.push(gasha1, gasha2, gasha3, gasha4, gasha5, gasha6, gasha7, gasha8, gasha9, gasha10)
 
-const categoriasFiltradas = allObjetc.filter((item) => item.categoria.includes(buscador));
+//const categoriasFiltradas = allObjetc.filter((item) => item.categoria.includes(buscador));
 
