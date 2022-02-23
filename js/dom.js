e15988602ae00f776c94f770195b4bd943b0b025
emiliasosa <emi.sosato@gmail.com>
2022-02-23 02:43:16 -0300

    Update dom.js

--- js/dom.js
+++ js/dom.js
@@ -9,3 +9,5 @@ let acumulador2 = JSON.parse(localStorage.getItem('Jugadas'))
 document.getElementById('cantidad').innerHTML = `Tienes un total de ${acumulador2} jugadas`;
 document.getElementById('premios').innerHTML = `Aun no tienes premios`;
 
+let user = JSON.parse(localStorage.getItem('Usuarios'))
+document.getElementById('bienvenida').innerHTML = `<h2 class='saludoPersonal'>¡Hola ${user.nombre}!</h2>`
